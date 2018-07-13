5732a8b50f03100b0d42e0fdf13b83580a0034d6
Alexis Gougaut <alex@macbook-pro-de-alexis.local>
2018-07-13 13:21:41 +0400

    Initial Commit

---
+++ Miam/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Miam
+//
+//  Created by Alexis Gougaut on 13/07/2018.
+//  Copyright © 2018 Alexis Gougaut. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
